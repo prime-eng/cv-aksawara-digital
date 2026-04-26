8570afb5fed47e23c980b1aebaf9d3a665b8b2f8
Dirlan <dirlan3781@gmail.com>
2026-04-27 03:34:12 +0700

    test root page

--- aksawara-next/app/page.tsx
+++ aksawara-next/app/page.tsx
@@ -1,21 +1,3 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import About from "@/components/About";
-import Services from "@/components/Services";
-import Portfolio from "@/components/Portfolio";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
-
 export default function Home() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <About />
-      <Services />
-      <Portfolio />
-      <Contact />
-      <Footer />
-    </>
-  );
+  return <h1>HELLO BERHASIL</h1>;
 }
